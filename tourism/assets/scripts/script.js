533182f70d1bd0c87e6d819fa3b28016e654dd63
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2023-03-05 13:18:05 +0300

    refactor

--- tourism/assets/scripts/script.js
+++ tourism/assets/scripts/script.js
@@ -2,18 +2,14 @@ let customSelect = document.getElementsByClassName("select__block");
 let customSelectCount = customSelect.length;
 
 for (let i = 0; i < customSelectCount; i++) {
-    //наш список select
     const select = customSelect[i].getElementsByTagName("select")[0];
-    //количество элеменьлв
     const countItemsSelect = select.length;
 
-    //select item block
     let containerSelect = document.createElement("div");
     containerSelect.classList.add("select__block--selected")
     containerSelect.innerHTML = select.options[select.selectedIndex].innerHTML;
     customSelect[i].appendChild(containerSelect);
 
-    // show/hide options list
     let selectList = document.createElement("div");
     selectList.className = "select__list select__list--hide";
 
@@ -21,6 +17,7 @@ for (let i = 0; i < customSelectCount; i++) {
         let item = document.createElement("div");
         item.classList.add("select__list-item")
         item.innerHTML = select.options[j].innerHTML;
+        item.setAttribute("data-select-index", select.options[j].value)
 
         if (j === select.selectedIndex) {
             item.classList.add("same-as-selected")
@@ -32,69 +29,38 @@ for (let i = 0; i < customSelectCount; i++) {
     customSelect[i].appendChild(selectList);
 
     containerSelect.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
         e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select__list--hide");
-        this.classList.toggle("select-arrow-active");
+        closeSelect(e.target);
+        e.target.nextSibling.classList.toggle("select__list--hide");
     });
 }
 
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select__list");
-    y = document.getElementsByClassName("select__block--selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select__list--hide");
+function closeSelect(e) {
+    let selectList = document.getElementsByClassName("select__list");
+    let selectedBlock = document.getElementsByClassName("select__block--selected");
+
+    for (let i = 0; i < selectedBlock.length; i++) {
+        if (e !== selectedBlock[i]) {
+            selectList[i].classList.add("select__list--hide");
         }
     }
 }
 
 function handleClickOnItem(e, select) {
-    var y
-    console.log("this: ", e.target)
-    // let parentSelect = e.target.parentNode.parentNode.querySelector("select");
-    console.log("parent: ", select)
-    let countOptions = select.length;
     let customSelectBlock = e.target.parentNode.previousSibling;
     let dropDownList = e.target.parentNode;
-    console.log("targer parent: ", e.target.parentNode)
 
-    for (let i = 0; i < countOptions; i++) {
-        if (select.options[i].innerHTML === e.target.innerHTML) {
-            select.selectedIndex = i;
-            customSelectBlock.innerHTML = e.target.innerHTML;
+    select.selectedIndex = e.target.getAttribute("data-select-index");
+    customSelectBlock.innerHTML = e.target.innerHTML;
 
-            y = dropDownList.querySelector(".same-as-selected");
-            console.log("same as selected", y)
-            // yl = y.length;
-            if (y) {
-                y.removeAttribute("class")
-            }
+    let selectedItem = dropDownList.querySelector(".same-as-selected");
 
-            // for (k = 0; k < yl; k++) {
-            //     y[k].removeAttribute("class");
-            // }
-            e.target.setAttribute("class", "same-as-selected");
-            break;
-        }
+    if (selectedItem) {
+        selectedItem.removeAttribute("class")
     }
+
+    e.target.setAttribute("class", "same-as-selected");
     customSelectBlock.click();
 }
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+document.addEventListener("click", closeSelect);
